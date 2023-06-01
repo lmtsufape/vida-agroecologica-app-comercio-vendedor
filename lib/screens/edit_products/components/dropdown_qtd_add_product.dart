09265cb5fa73163eae7f77f90f109b276f180849
Bielmelo6 <gabrielmelodemenezes@gmail.com>
2023-06-01 15:45:30 -0300

    Mudança no design na tela AddProductsScreen

--- lib/screens/edit_products/components/dropdown_qtd_add_product.dart
+++ lib/screens/edit_products/components/dropdown_qtd_add_product.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:thunderapp/screens/edit_products/components/stock_add_product.dart';
 import 'package:thunderapp/shared/constants/app_number_constants.dart';
 import 'package:thunderapp/shared/constants/style_constants.dart';
 
@@ -16,58 +17,64 @@ class DropDownQtdAddProduct extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        const Padding(
-          padding: EdgeInsets.only(bottom: 4),
-          child: Text(
-            'Un.Med.',
-            style: TextStyle(color: kTextButtonColor),
-          ),
-        ),
-        Container(
-          alignment: AlignmentDirectional.centerStart,
-          height: size.height * 0.06,
-          width: size.width * 0.25,
-          child: ValueListenableBuilder(
-              valueListenable: dropValue,
-              builder:
-                  (BuildContext context, String value, _) {
-                return DropdownButtonFormField<String>(
-                  isExpanded: true,
-                  decoration: const InputDecoration(
-                    fillColor: Colors.white,
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(6),
-                      ),
-                      borderSide: BorderSide(
-                          color: kTextButtonColor),
-                    ),
-                  ),
-                  icon: Icon(
-                    Icons.keyboard_arrow_down,
-                    color: kPrimaryColor,
-                    size: size.width * 0.05,
-                  ),
-                  hint: Text('Unidade'),
-                  value: (value.isEmpty) ? null : value,
-                  onChanged: (escolha) =>
-                      dropValue.value = escolha.toString(),
-                  items: dropOpcoes
-                      .map(
-                        (op) => DropdownMenuItem(
-                          child: Text(op),
-                          value: op,
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(bottom: 4),
+              child: Text(
+                'Unidade de medida',
+                style: TextStyle(color: kTextButtonColor),
+              ),
+            ),
+            Container(
+              alignment: AlignmentDirectional.centerStart,
+              height: size.height * 0.06,
+              width: size.width * 0.57,
+              child: ValueListenableBuilder(
+                  valueListenable: dropValue,
+                  builder:
+                      (BuildContext context, String value, _) {
+                    return DropdownButtonFormField<String>(
+                      isExpanded: true,
+                      decoration: const InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(6),
+                          ),
+                          borderSide: BorderSide(
+                              color: kTextButtonColor),
                         ),
-                      )
-                      .toList(),
-                );
-              }),
+                      ),
+                      icon: Icon(
+                        Icons.keyboard_arrow_down,
+                        color: kPrimaryColor,
+                        size: size.width * 0.05,
+                      ),
+                      hint: Text('Unidade'),
+                      value: (value.isEmpty) ? null : value,
+                      onChanged: (escolha) =>
+                          dropValue.value = escolha.toString(),
+                      items: dropOpcoes
+                          .map(
+                            (op) => DropdownMenuItem(
+                              child: Text(op),
+                              value: op,
+                            ),
+                          )
+                          .toList(),
+                    );
+                  }),
+            ),
+          ],
         ),
+        StockAddProduct(),
       ],
     );
   }
