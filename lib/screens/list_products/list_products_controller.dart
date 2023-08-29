a6119362154e97d9bd7af793598328fc99b9d6de
Bielmelo6 <gabrielmelodemenezes@gmail.com>
2023-08-29 07:58:14 -0300

    ListProductsController new controller

---
+++ lib/screens/list_products/list_products_controller.dart
@@ -0,0 +1,51 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:thunderapp/screens/products/products_repository.dart';
+import 'package:thunderapp/shared/core/models/products_model.dart';
+
+import '../list_products/components/card_products_list.dart';
+
+class ListProductsController extends GetxController {
+  List<CardProductsList> products = [];
+  int quantProducts = 0;
+  int quantStock = 0;
+  ProductsRepository repository = ProductsRepository();
+  final TextEditingController _searchController =
+  TextEditingController();
+
+  TextEditingController get searchController =>
+      _searchController;
+
+  Future<List<CardProductsList>> populateCardsProductsList() async {
+    List<CardProductsList> list = [];
+
+    var products = await repository.getProducts();
+
+    quantProducts = products.length;
+
+    for (int i = 0; i < products.length; i++) {
+      CardProductsList card = CardProductsList(products[i]);
+      list.add(card);
+      if (products.isNotEmpty) {
+        quantStock = products[i].estoque!;
+      }
+    }
+
+    if (list.isNotEmpty) {
+      update();
+      return list;
+    } else {
+      log('CARD VAZIO');
+      return list;
+    }
+  }
+
+  @override
+  void onInit() async {
+    products = await populateCardsProductsList();
+    super.onInit();
+    update();
+  }
+}
