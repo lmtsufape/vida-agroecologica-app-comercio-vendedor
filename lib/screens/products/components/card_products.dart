c82b870f8b8b4e6347f6db9d07f288b698c5216f
Bielmelo6 <gabrielmelodemenezes@gmail.com>
2023-08-29 07:44:04 -0300

    CardProducts new design

--- lib/screens/products/components/card_products.dart
+++ lib/screens/products/components/card_products.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:thunderapp/screens/add_products/add_products_screen.dart';
 
 import 'package:thunderapp/shared/constants/style_constants.dart';
 import 'package:thunderapp/shared/core/models/products_model.dart';
 
+import '../../../shared/core/navigator.dart';
+import '../../screens_index.dart';
+
 class CardProducts extends StatefulWidget {
   ProductsModel model;
+
   CardProducts(this.model, {Key? key}) : super(key: key);
 
   @override
@@ -15,51 +20,55 @@ class _CardProductsState extends State<CardProducts> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return SizedBox(
-      width: size.width,
-      height: size.height * 0.066,
-      child: Card(
-        elevation: 4,
-        color: Colors.white,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.zero,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: const EdgeInsetsDirectional.only(
-                  start: 14),
-              child: Text(
-                widget.model.nome.toString(),
-                style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: size.height * 0.024),
-              ),
+    return Column(
+      children: [
+        SizedBox(
+          width: size.width,
+          height: size.height * 0.07,
+          child: Card(
+            margin: EdgeInsets.zero,
+            elevation: 0.7,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.zero,
             ),
-            Padding(
-              padding:
-                  const EdgeInsetsDirectional.only(end: 14),
-              child: Column(
-                children: [
-                  Text(
-                    'Quantidade',
-                    style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: size.height * 0.016,
-                        color: Colors.grey),
-                  ),
-                  Text(widget.model.estoque.toString(),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsetsDirectional.only(start: 100),
+                  child: Container(
+                    width: size.width * 0.4,
+                    alignment: Alignment.center,
+                    child: Text(
+                      widget.model.nome.toString(),
                       style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                          fontSize: size.height * 0.024,
-                          color: kPrimaryColor)),
-                ],
-              ),
+                          fontWeight: FontWeight.w600,
+                          fontSize: size.height * 0.020),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsetsDirectional.only(end: 14),
+                  child: IconButton(
+                    onPressed: () {
+                      //Navigator.push(context, MaterialPageRoute(builder: (context) {
+                        //return AddProductsScreen();
+                      //}),);
+                      Navigator.pushNamed(context, Screens.listProducts);
+                    },
+                    icon: Icon(
+                      Icons.add_circle_outline_outlined,
+                      size: size.height * 0.045,
+                      color: kPrimaryColor,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
-      ),
+        Divider(height: size.height * 0.01, color: Colors.transparent,)
+      ],
     );
   }
 }
