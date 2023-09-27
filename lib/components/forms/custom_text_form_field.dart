41096736a90396c39b6fab05195665f75f4128e8
Bielmelo6 <gabrielmelodemenezes@gmail.com>
2023-09-26 22:25:08 -0300

    novo componente CustomTextFormCurrency

--- lib/components/forms/custom_text_form_field.dart
+++ lib/components/forms/custom_text_form_field.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
 
 
 class CustomTextFormField extends StatefulWidget {
@@ -76,6 +79,92 @@ class _CustomTextFormFieldState
                         : Icons.visibility_off,
                   ),
                 )
+              : null,
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+class CustomTextFormFieldCurrency extends StatefulWidget {
+  const CustomTextFormFieldCurrency(
+      {Key? key,
+        this.onChanged,
+        this.label,
+        this.currencyFormatter,
+        this.controller,
+        this.keyboardType,
+        this.hintText,
+        this.isPassword,
+        this.icon,
+        this.isBordered})
+      : super(key: key);
+
+  final Function(String)? onChanged;
+  final String? label;
+  final List<TextInputFormatter>? currencyFormatter;
+  final String? hintText;
+  final TextEditingController? controller;
+  final TextInputType? keyboardType;
+  final bool? isPassword;
+  final IconData? icon;
+  final bool? isBordered;
+  @override
+  State<CustomTextFormFieldCurrency> createState() =>
+      _CustomTextFormFieldCurrencyState();
+}
+
+class _CustomTextFormFieldCurrencyState
+    extends State<CustomTextFormFieldCurrency> {
+  bool _obscureText = false;
+  @override
+  void initState() {
+    if (widget.isPassword != null) {
+      _obscureText = widget.isPassword!;
+    }
+    super.initState();
+  }
+
+  void _toggleVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: TextFormField(
+        onChanged: widget.onChanged,
+        inputFormatters: widget.currencyFormatter == null
+            ? null
+            : widget.currencyFormatter!,
+        obscureText: _obscureText,
+        controller: widget.controller,
+        decoration: InputDecoration(
+          prefixIcon: widget.icon == null
+              ? null
+              : Icon(widget.icon),
+          border: widget.isBordered == null
+              ? InputBorder.none
+              : const OutlineInputBorder(),
+          labelText: widget.label,
+          filled: true,
+          fillColor: Colors.white,
+          hintText: widget.hintText,
+          suffixIcon: widget.isPassword == true
+              ? InkWell(
+            onTap: () => _toggleVisibility(),
+            child: Icon(
+              _obscureText
+                  ? Icons.visibility
+                  : Icons.visibility_off,
+            ),
+          )
               : null,
         ),
       ),
