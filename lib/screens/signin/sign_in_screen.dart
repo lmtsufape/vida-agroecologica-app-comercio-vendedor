0005452da75e27ca55646617d41b94b21ac80130
Bielmelo6 <gabrielmelodemenezes@gmail.com>
2023-10-18 15:43:03 -0300

    Mudança no front end da tela de login

--- lib/screens/signin/sign_in_screen.dart
+++ lib/screens/signin/sign_in_screen.dart
@@ -11,6 +11,7 @@ import 'package:thunderapp/shared/constants/app_number_constants.dart';
 import 'package:thunderapp/shared/constants/style_constants.dart';
 import 'package:thunderapp/shared/core/navigator.dart';
 
+import '../../assets/index.dart';
 import '../../shared/constants/app_enums.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -21,98 +22,176 @@ class SignInScreen extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return MultiProvider(
         providers: [
-          ChangeNotifierProvider(
-              create: (_) => SignInController()),
+          ChangeNotifierProvider(create: (_) => SignInController()),
         ],
         builder: (context, child) {
           return Consumer<SignInController>(
-            builder: (context, controller, child) =>
-                Scaffold(
+            builder: (context, controller, child) => Scaffold(
               backgroundColor: kPrimaryColor,
               body: Stack(
                 children: [
                   Container(
                     width: size.width,
-                    margin: EdgeInsets.only(
-                        top: size.height * 0.4),
-                    padding: const EdgeInsets.all(
-                        kDefaultPadding),
+                    margin: EdgeInsets.only(top: size.height * 0.4),
+                    padding: const EdgeInsets.only(top: 20, left: 28, bottom: 0, right: 28),
                     decoration: BoxDecoration(
                         color: kBackgroundColor,
-                        borderRadius:
-                            BorderRadius.circular(30)),
+                        borderRadius: BorderRadius.circular(30)),
                     child: Column(
-                      mainAxisAlignment:
-                          MainAxisAlignment.center,
-                      crossAxisAlignment:
-                          CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const Spacer(),
+                        Divider(
+                            height: size.height * 0.01,
+                            color: Colors.transparent),
                         Center(
                           child: Text(
                             'Entrar',
-                            style: kTitle1.copyWith(
-                                fontWeight:
-                                    FontWeight.bold),
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: size.height * 0.032),
                           ),
                         ),
-                        const VerticalSpacerBox(
-                            size: SpacerSize.huge),
-                        CustomTextFormField(
+                        Divider(height: size.height * 0.02, color: Colors.transparent,),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'E-mail',
+                              style: TextStyle(
+                                  color: kSecondaryColor,
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: size.height * 0.018),
+                            ),
+                            IntrinsicWidth(
+                              stepWidth: size.width,
+                              child: Card(
+                                margin: EdgeInsets.zero,
+                                elevation: 0,
+                                child: ClipPath(
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                      border: Border(
+                                        bottom: BorderSide(
+                                            color: Colors.black, width: 1),),
+                                    ),
+                                    alignment: Alignment.center,
+                                    child: CustomTextFormField(
+                                      controller: controller.emailController,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        /*CustomTextFormField(
                           hintText: 'Seu email',
                           icon: Icons.email,
-                          controller:
-                              controller.emailController,
+                          controller: controller.emailController,
+                        ),*/
+                        const VerticalSpacerBox(size: SpacerSize.small),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Senha',
+                              style: TextStyle(
+                                  color: kSecondaryColor,
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: size.height * 0.018),
+                            ),
+                            IntrinsicWidth(
+                              stepWidth: size.width,
+                              child: Card(
+                                margin: EdgeInsets.zero,
+                                elevation: 0,
+                                child: ClipPath(
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                      border: Border(
+                                        bottom: BorderSide(
+                                            color: Colors.black, width: 1),),
+                                    ),
+                                    alignment: Alignment.center,
+                                    child: CustomTextFormField(
+                                      controller: controller.passwordController,
+                                      isPassword: true,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
                         ),
-                        const VerticalSpacerBox(
-                            size: SpacerSize.small),
-                        CustomTextFormField(
+                        /*CustomTextFormField(
                           hintText: 'Sua senha',
                           isPassword: true,
                           icon: Icons.lock,
-                          controller:
-                              controller.passwordController,
-                        ),
+                          controller: controller.passwordController,
+                        ),*/
                         Align(
                           alignment: Alignment.centerRight,
                           child: TextButton(
                               onPressed: () {},
                               child: const Text(
-                                  'Esqueceu a senha?')),
+                                'Esqueceu a senha?',
+                                style: TextStyle(color: kPrimaryColor),
+                              )),
                         ),
-                        const VerticalSpacerBox(
-                            size: SpacerSize.medium),
-                        controller.status ==
-                                SignInStatus.loading
-                            ? const Center(
-                                child:
-                                    CircularProgressIndicator())
+                        const VerticalSpacerBox(size: SpacerSize.medium),
+                        controller.status == SignInStatus.loading
+                            ? const Center(child: CircularProgressIndicator())
                             : PrimaryButton(
                                 text: 'Entrar',
                                 onPressed: () {
-                                  controller
-                                      .signIn(context);
+                                  controller.signIn(context);
                                 }),
-                        const VerticalSpacerBox(
-                            size: SpacerSize.large),
+                        const VerticalSpacerBox(size: SpacerSize.large),
                         SizedBox(
                           width: size.width,
                           child: Column(
-                            crossAxisAlignment:
-                                CrossAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Desenvolvido por',
+                                style: TextStyle(
+                                    fontSize: size.height * 0.018,
+                                    fontWeight: FontWeight.w700,
+                                    color: kSecondaryColor),
+                              ),
+                              Divider(height: size.height * 0.018, color: Colors.transparent,),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset(
+                                    Assets.ufape,
+                                    height: size.height * 0.09,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 8),
+                                    child: Image.asset(
+                                      Assets.lmts,
+                                      height: size.height * 0.06,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: size.width,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
                             children: <Widget>[
-                              controller.errorMessage !=
-                                      null
+                              controller.errorMessage != null
                                   ? Text(
-                                      controller
-                                          .errorMessage!,
-                                      style: kCaption1
-                                          .copyWith(
-                                              color:
-                                                  kErrorColor),
+                                      controller.errorMessage!,
+                                      style: kCaption1.copyWith(
+                                          color: kErrorColor),
                                     )
                                   : const SizedBox(),
-                          
                             ],
                           ),
                         ),
@@ -121,20 +200,39 @@ class SignInScreen extends StatelessWidget {
                     ),
                   ),
                   Column(
-                    mainAxisAlignment:
-                        MainAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Align(
                         alignment: Alignment.bottomCenter,
-                        child: Text(
-                          textAlign: TextAlign.center,
-                          'Bem-vindo(a) ao App Bonito Produtor',
-                          style: kTitle1.copyWith(
-                              color: kBackgroundColor),
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              Assets.logo,
+                              width: size.width * 0.5,
+                              color: kBackgroundColor,
+                            ),
+                            Text(
+                              textAlign: TextAlign.center,
+                              'VIDA AGROECOLÓGICA',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: size.height * 0.032,
+                                  color: kBackgroundColor),
+                            ),
+                            Text(
+                              textAlign: TextAlign.center,
+                              '(aplicativo agricultor/a)',
+                              style: TextStyle(
+                                fontSize: size.height * 0.024,
+                                fontWeight: FontWeight.w500,
+                                color: kBackgroundColor,
+                              ),
+                            )
+                          ],
                         ),
                       ),
                       SizedBox(
-                        height: size.height * 0.5,
+                        height: size.height * 0.56,
                       )
                     ],
                   ),
