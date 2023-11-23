ea3f05c543032c52fa5ee4e6020dc7f327f7f1d5
Bielmelo6 <gabrielmelodemenezes@gmail.com>
2023-11-23 13:18:42 -0300

    mudanças e melhorias no componente de imagem do AddProducts

--- lib/screens/add_products/components/image_edit.dart
+++ lib/screens/add_products/components/image_edit.dart
@@ -1,6 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:thunderapp/screens/add_products/add_products_controller.dart';
 import '../../../shared/constants/app_text_constants.dart';
+import '../../../shared/constants/style_constants.dart';
 
 class ImageEdit extends StatefulWidget {
   final AddProductsController? controller;
@@ -36,7 +38,8 @@ class _ImageEditState extends State<ImageEdit> {
 
   Future<void> _loadImage() async {
     try {
-      widget.controller!.setHasImage(await widget.controller!.boolImage(_currentProductId));
+      widget.controller!.setHasImage(
+          await widget.controller!.boolImage(_currentProductId));
       setState(() {
         _loading = false;
       });
@@ -51,7 +54,10 @@ class _ImageEditState extends State<ImageEdit> {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+    Size size = MediaQuery
+        .of(context)
+        .size;
+
     return Container(
       alignment: Alignment.center,
       height: size.width * 0.4,
@@ -60,21 +66,21 @@ class _ImageEditState extends State<ImageEdit> {
         child: Material(
           elevation: 3,
           shadowColor: Colors.black,
-          child: _loading
-              ? CircularProgressIndicator() // Display a loading indicator while loading the image.
-              : widget.controller?.hasImage ?? false
-              ? Image.network(
-            '$kBaseURL/produtos/$_currentProductId/imagem',
-            headers: {
-              "Content-Type": "application/json",
-              "Accept": "application/json",
-              "Authorization": "Bearer ${widget.controller?.token}"
-            },
-          )
-              : Icon(
-            Icons.shopping_bag,
-            size: 100,
-            color: Colors.orange,
+          child: CachedNetworkImage(
+              imageUrl: '$kBaseURL/produtos/$_currentProductId/imagem',
+              httpHeaders: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization":
+                "Bearer ${widget.controller?.token}"
+              },
+              errorWidget: (context, url, error) =>
+                  Icon(
+                    Icons.shopping_bag,
+                    size: size.height * 0.1,
+                    color: kPrimaryColor,
+                  ),
+              placeholder: (context, url) => const CircularProgressIndicator(color: kPrimaryColor,),
           ),
         ),
       ),
