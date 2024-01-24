ebc1f1c2624691e0be735b0efedaa2f23adf34d9
Bielmelo6 <gabrielmelodemenezes@gmail.com>
2024-01-24 17:22:56 -0300

    chave ssl

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:logging/logging.dart';
 import 'package:thunderapp/app.dart';
+import 'dart:io';
 import 'package:timezone/data/latest_all.dart' as tz;
 
-main() {
+main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  ByteData data =
+  await PlatformAssetBundle().load('lib/assets/images/ufape-edu-br.pem');
+  SecurityContext.defaultContext
+      .setTrustedCertificatesBytes(
+      data.buffer.asUint8List());
   tz.initializeTimeZones();
   Logger.root.level = Level.ALL; // defaults to Level.INFO
   Logger.root.onRecord.listen((record) {});
